5e8a30cbb2cf8dd7b55cfdb497ac458ae87aced2
Saikumarkollipara <krksaikumar@gmail.com>
2023-09-11 11:43:33 +0530

    debugging

--- src/server.js
+++ src/server.js
@@ -11,31 +11,37 @@ app.get('/original-image', getOriginalImageFromBing);
 
 app.listen(PORT, () => `Server listening on port: ${PORT}`);
 
-export async function getOriginalImageFromBing(req, res) {
-  const BASE_URL = 'https://www.bing.com';
-  const { url } = req.query;
-  const browser = await puppeteer.launch({
+// Create a global variable to hold the browser instance
+let browser;
+
+// Initialize the browser instance once when the server starts
+(async () => {
+  browser = await puppeteer.launch({
     args: [
       '--disable-setuid-sandbox',
       '--no-sandbox',
       '--single-process',
       '--no-zygote',
     ],
-    headless: 'true',
+    headless: true,
     executablePath:
       process.env.NODE_ENV === 'production'
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
   });
+})();
+
+export async function getOriginalImageFromBing(req, res) {
+  const BASE_URL = 'https://www.bing.com';
+  const { url } = req.query;
+  let page;
   try {
-    console.log('browser created');
-    const page = await browser.newPage();
+    page = await browser.newPage();
     console.log('new page opened');
-    await page.goto(BASE_URL + url, { timeout: 150000 });
+    // await page.goto(BASE_URL + url, { waitUntil: 'load' });
+    await page.goto(BASE_URL + url);
     console.log('got the page');
     await page.waitForSelector('.imgContainer img.nofocus', { timeout: 5000 });
-    console.log('got the selector');
-
     const imgSrc = await page.$eval(
       '.imgContainer img.nofocus',
       (img) => img.src
@@ -46,6 +52,8 @@ export async function getOriginalImageFromBing(req, res) {
     console.log(err);
     res.status(500).json({ success: false, message: 'Web scraper error' });
   } finally {
-    await browser.close();
+    if (page) {
+      await page.close();
+    }
   }
 }
